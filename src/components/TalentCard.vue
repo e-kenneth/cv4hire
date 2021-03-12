053a825da53cb331e2c538055a2e211bf4976380
Vicktor Hutama <vicktor.hutama@gmail.com>
2021-03-12 16:31:09 +0700

    browse

---
+++ src/components/TalentCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container q-ma-md q-pa-md">
+      <div class="row">
+          <div>{{talent.name}}</div>
+          <div>{{talent.name}}</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    talent: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+    .container{
+        background-color: $accent;
+        border-radius: 5px;
+    }
+</style>
