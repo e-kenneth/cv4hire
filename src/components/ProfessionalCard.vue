63674dc7d4d617b5b1c8db5a2879f255dcedc640
Vicktor Hutama <vicktor.hutama@gmail.com>
2021-06-28 15:47:20 +0700

    UI updates. still need refactoring

--- src/components/ProfessionalCard.vue
+++ src/components/ProfessionalCard.vue
@@ -22,17 +22,25 @@
             :offset="[10, 10]"
             class="bg-white text-secondary"
           >
-            TBD To be done later 
+            Kode professional yang berfungsi sebagai identitas di CVForHire sebagai pengganti nama 
           </q-tooltip>
         </div>
       </div>
-      <div class="titleContainer">
+      <div class="titleContainer q-px-md">
         <div class="title">{{ professional.username }}</div>
       </div>
-      <div class="q-px-md q-py-sm">
+      <div class="titleExplanationContainer2">
+        <div class="titleExplanation2">SPESIALISASI</div>
+      </div>
+      <div class="q-px-md">
         <div class="subtitle">{{ jobs }}</div>
       </div>
-      <div class="subtitle2">{{ cities }}</div>
+      <div class="titleExplanationContainer2">
+        <div class="titleExplanation2">BERSEDIA DITEMPATKAN DI</div>
+      </div>
+      <div class="q-px-md">
+        <div class="subtitle2">{{ cities }}</div>
+      </div>
     </router-link>
   </div>
 </template>
@@ -52,11 +60,11 @@ export default {
       cities.forEach((city) => {
         this.$store.state.main.options.cities.forEach((cityState) => {
           if (city == cityState.value) {
-            stringCities += cityState.label + ", ";
+            stringCities += cityState.label + " | ";
           }
         });
       });
-      stringCities = stringCities.slice(0, -2);
+      stringCities = stringCities.slice(0, -3);
       return stringCities;
     },
     jobs() {
@@ -65,11 +73,11 @@ export default {
       jobs.forEach((job) => {
         this.$store.state.main.options.jobs.forEach((jobState) => {
           if (job == jobState.value) {
-            stringJobs += jobState.label + ", ";
+            stringJobs += jobState.label + " | ";
           }
         });
       });
-      stringJobs = stringJobs.slice(0, -2);
+      stringJobs = stringJobs.slice(0, -3);
       return stringJobs;
     },
   },
@@ -119,8 +127,8 @@ a {
 }
 .titleExplanation {
   text-align: center;
-  color: $secondary;
-  background-color: white;
+  color: white;
+  background-color: $secondary;
   margin-top: 10px;
   margin-bottom: -10px;
   padding: 5px;
@@ -133,11 +141,11 @@ a {
 }
 .title {
   text-align: center;
-  color: white;
-  background-color: $secondary;
+  color: $secondary;
+  background-color: white;
   padding: 5px;
   font-size: 30px;
-  display: inline-block;
+  // display: inline-block;
   text-align: center;
   border-radius: 5px;
   box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.5);
@@ -149,11 +157,29 @@ a {
   padding: 5px;
   font-size: 15px;
   border-radius: 5px;
+  box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.5);
 }
 .subtitle2 {
   text-align: center;
-  color: rgb(255, 255, 255);
-  padding: 0 15px 5px 15px;
+  color: $secondary;
+  background-color: white;
+  padding: 5px;
   font-size: 15px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.5);
+}
+.titleExplanationContainer2 {
+  text-align: center;
+}
+.titleExplanation2 {
+  text-align: center;
+  color: white;
+  background-color: $secondary;
+  margin-top: 10px;
+  margin-bottom: -10px;
+  padding: 5px;
+  border-radius: 5px;
+  font-size: 10px;
+  display: inline-block;
 }
 </style>
