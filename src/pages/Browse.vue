83956e69956f13021e5b044ed28e935e49543776
Vicktor Hutama <vicktor.hutama@gmail.com>
2021-06-28 14:59:42 +0700

    Implementing sliding filter

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
+    <div class="row q-pt-md justify-center">
+      <q-btn color="secondary" icon="filter_list" label="Filter" @click="folded = !folded" />
+    </div>
+    <div class="row q-pt-md q-pa-md justify-center">
+      <div class="filterContainer q-ml-sm q-mr-sm" :class="{'folded' : folded}" >Filter</div>
+    </div>
     <div class="row q-pa-md items-stretch">
-      <!-- {{professionals}} -->
       <div
         class="professionals col-6 col-lg-2"
         v-for="professional in professionals"
@@ -26,6 +31,7 @@ export default {
   data() {
     return {
       professionals: [],
+      folded: true,
     };
   },
   computed: {
@@ -68,5 +74,15 @@ export default {
 .professionals {
   // align-items: stretch;
   margin-bottom: 20px;
+}
+.filterContainer{
+  background-color: $primary;
+  width: 100%;
+  height: 400px;
+  border-radius: 5px;
+  transition: height .5s;
+}
+.filterContainer.folded{
+  height: 0px;
 }
 </style>
