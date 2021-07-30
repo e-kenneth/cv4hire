6b03797e7a95240a299dc51583652a86d413ebc2
Ernest kenneth <ernestkennethyoungsumarno@gmail.com>
2021-07-30 16:48:28 +0700

    Making faq pages (proffesional)

---
+++ src/components/FaqCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-card class="my-card" flat bordered>
+    <q-img :src="card.img" height="400px"/>
+
+    <q-card-section>
+      <div class="text-h5 q-mt-sm q-mb-xs">{{ card.title }}</div>
+      <div class="text-caption text-grey">
+        {{ card.text1 }}
+      </div>
+    </q-card-section>
+    <q-card-actions>
+      <q-space />
+
+      <q-btn
+        color="grey"
+        round
+        flat
+        dense
+        :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+        @click="expanded = !expanded"
+      />
+    </q-card-actions>
+    <q-slide-transition>
+      <div v-show="expanded">
+        <q-separator />
+        <q-card-section class="text-subitle2">
+          {{ card.text2 }}
+        </q-card-section>
+      </div>
+    </q-slide-transition>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    card: Object,
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/Professional/Faq.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="header text-center">Cara mencari kerja di CVForHire</div>
+
+    <div class="row q-pa-md q-gutter-md">
+        
+      <div class="col" v-for="card in faqCards" :key="card.title">
+        <FaqCard :card="card"></FaqCard>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import FaqCard from "components/FaqCard.vue";
+export default {
+    components: {
+        FaqCard,
+    },
+  data() {
+    return {
+        faqCards: [
+            {img: "https://i.imgur.com/vikAgTO.jpg", title:"Membuat akun baru", text1: "Pencari kerja membuat akun baru untuk mendapatkan nomor ID", text2: "Text2"},
+            {img: "https://i.imgur.com/1vhQu7c.jpg", title:"Buat video  ", text1: "Pencari kerja membuat video dengan melihat panduan yang telah disediakan oleh CVforhire", text2: "Text2"},
+            {img: "https://i.imgur.com/qUf2d9y.jpg", title:"Kirimkan video", text1: "Pencari kerja bisa mengirimkan video dalam format MP4 kepada admin CVforhire", text2: "Text2"},
+        ],
+      step: 1,
+      expanded: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+    font-size: 50px;
+}
+</style>
